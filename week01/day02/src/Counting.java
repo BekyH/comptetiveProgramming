99d5385443f42180fc136949e0d3b7e32f0410e2
unknown <bereketheramo25@gmail.com>
2021-01-12 14:52:58 +0300

    modification in counting

--- week01/day02/src/Counting.java
+++ week01/day02/src/Counting.java
@@ -6,24 +6,36 @@ public class Counting {
         int n = arr.length;
 
         ArrayList<Integer> arrayList = new ArrayList<>();
+        ArrayList<Integer> negativeNums = new ArrayList<>();
         int max = 0;
-
+        for(int ii=0;ii<n;ii++){
+            if(arr[ii]>max){
+                max=arr[ii];
+            }
+            if(arr[ii]<0){
+                negativeNums.add(arr[ii]);
+            }
+        }
         int x = max + 1;
-        if(x<n){
-            int diff = n-x;
-            x=x+diff;
+        System.out.println(max);
+        if(x<n) {
+            int diff = n - x;
+            x = x + diff;
         }
 
         int[] sortres = new int[x];
-        for (int j = 0; j < x; j++) {
-            int temp = arr[j];
+        for (int j = 0; j < n; j++) {
+            if(arr[j]>=0){
+                int temp = arr[j];
 
-            if(sortres[temp]>0){
-                sortres[temp]++;
+                if(sortres[temp]>0){
+                    sortres[temp]++;
+                }
+                else{
+                    sortres[temp] = 1;
+                }
             }
-            else{
-                sortres[temp] = 1;
-            }
+
 
 
 
@@ -35,11 +47,11 @@ public class Counting {
 
         }
         for(int f=0;f<sortres.length;f++){
-           int ff = sortres[f];
-           while (ff>0){
-               arrayList.add(f);
-               ff--;
-           }
+            int ff = sortres[f];
+            while (ff>0){
+                arrayList.add(f);
+                ff--;
+            }
         }
         System.out.println();
         for(int e:arrayList){
