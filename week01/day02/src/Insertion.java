fe34d466aac55ed19d1786573ab873a29d363483
unknown <bereketheramo25@gmail.com>
2021-01-13 03:51:54 +0300

    insertion sort

---
+++ week01/day02/src/Insertion.java
@@ -0,0 +1,25 @@
+public class Insertion {
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6};
+        insertionSort(arr);
+    }
+
+    public static void insertionSort(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+
+                int key = arr[i];
+                int j = i-1;
+                while(j>=0 && arr[j]>key){
+                        arr[j+1]=arr[j];
+                        j--;
+                }
+                arr[j+1]=key;
+        }
+        for(int x:arr){
+            System.out.println(x + " ");
+        }
+    }
+}
+
