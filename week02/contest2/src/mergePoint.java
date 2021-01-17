ea6dc32672dfcd6a83806dab4ab5b8160ab1a71a
unknown <bereketheramo25@gmail.com>
2021-01-17 08:04:49 +0300

    another approach added

--- week02/contest2/src/mergePoint.java
+++ week02/contest2/src/mergePoint.java
@@ -1,5 +1,6 @@
 import com.sun.scenario.effect.Merge;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class mergePoint {
@@ -17,6 +18,9 @@ public static void main(String[] args){
     n3.next = n6;
     n4.next = n5;
     int p = Merge(n1,n4);
+    int x = merge2(n1,n4);
+    System.out.println(x);
+
     System.out.println(p);
 
 
@@ -43,6 +47,27 @@ public static void main(String[] args){
         return node1.data;
 
     }
+    // another approach 
+
+    public static int merge2(Node head1,Node head2){
+        HashMap<Node,Integer> hashMap = new HashMap<>();
+        Node node1 = head1;
+        Node node2 = head2;
+        while(node1!=null){
+            hashMap.put(node1,1);
+            node1 = node1.next;
+        }
+        for(Map.Entry<Node,Integer> e:hashMap.entrySet()){
+            while(node2!=null){
+                if(hashMap.containsKey(node2)){
+                    return node2.data;
+                }
+                node2 = node2.next;
+            }
+        }
+        return 0;
+    }
+
 }
 class Node {
 
