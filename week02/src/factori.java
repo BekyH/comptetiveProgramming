e1959f099bd2ffb0372431ca4682b2292f27654d
unknown <bereketheramo25@gmail.com>
2021-01-19 10:26:08 +0300

    factorial

---
+++ week02/src/factori.java
@@ -0,0 +1,14 @@
+public class factori {
+
+    public static void main(String[] args){}
+    static int factorial(int n) {
+        if(n==0 || n ==1) {
+            return 1;
+        }
+
+            return n * factorial(n-1);
+        }
+
+
+    }
+}
