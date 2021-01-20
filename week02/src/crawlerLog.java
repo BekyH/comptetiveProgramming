a77e9b2fe5f1a789df405e3295018f32c9ca662a
unknown <bereketheramo25@gmail.com>
2021-01-20 17:40:06 +0300

    crawler log folder

---
+++ week02/src/crawlerLog.java
@@ -0,0 +1,23 @@
+public class crawlerLog {
+
+    public int minOperations(String[] logs) {
+        int counter = 0;
+        String back = "../";
+        String remain="./";
+        for(int i = 0;i<logs.length;i++){
+            if(!logs[i].equals(back) && !logs[i].equals(remain)){
+                counter++;
+            }
+            else if(logs[i].equals(back)){
+                if(counter>0){
+                    counter = counter-1;
+                }
+            }
+            else{
+
+            }
+        }
+        return counter;
+    }
+
+}
