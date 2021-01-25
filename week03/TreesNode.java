1a52d5b121cf044179575a6c8f4c779ffbd8208f
unknown <bereketheramo25@gmail.com>
2021-01-25 08:39:10 +0300

    postorder traversal

--- week03/TreesNode.java
+++ week03/TreesNode.java
@@ -1,23 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
 
-
-  public class TreesNode {
+public class TreesNode {
     public static void main(String args[]){
 
     }
-      int val;
-      TreesNode left;
-      TreesNode right;
-      TreesNode() {}
-      TreesNode(int val) { this.val = val; }
-      TreesNode(int val, TreesNode left, TreesNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
+    int val;
+    TreesNode left;
+    TreesNode right;
+    TreesNode() {}
+    TreesNode(int val) { this.val = val; }
+    TreesNode(int val, TreesNode left, TreesNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 
-  }
+}
 
 class Solution {
+    List<Integer> res;
+    public List<Integer> postorderTraversal(TreesNode root) {
+        List<Integer> res = new ArrayList<>();
+
+        traverse(root,res);
+
+        return res;
+
+
+    }
+    public void traverse(TreesNode root, List<Integer>res){
+        if(root!=null) {
+            traverse(root.left, res);
+
+            traverse(root.right, res);
+            res.add(root.val);
+
+        }
+    }
     public TreesNode searchBST(TreesNode root, int val) {
         TreesNode current = root;
         while(current.val!=val){
@@ -38,4 +58,5 @@ class Solution {
 
         return current;
     }
+
 }
