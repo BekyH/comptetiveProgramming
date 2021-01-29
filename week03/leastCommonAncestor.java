4adb9aa71a313ceb257e8c1ab77288354b73f621
unknown <bereketheramo25@gmail.com>
2021-01-29 05:14:29 +0300

    least common ancestor

---
+++ week03/leastCommonAncestor.java
@@ -0,0 +1,27 @@
+public class leastCommonAncestor {
+    TreeNode lca = null;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        int p_val = p.val;
+        int q_val = q.val;
+        int root_val = root.val;
+        if (root_val > p_val && root_val > q_val){
+            lowestCommonAncestor(root.left, p, q);
+        }
+        else if(root_val < p_val && root_val < q_val){
+            lowestCommonAncestor(root.right, p, q);
+        }
+        else{
+            lca = root;
+            return lca;
+        }
+        return lca;
+    }
+
+
+}
+ class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
