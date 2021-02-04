df614d1068455e8601f85739864a23a2614484a4
unknown <bereketheramo25@gmail.com>
2021-02-04 05:01:11 +0300

    height of tree

--- week03/contest03/heightofTree.java
+++ week03/contest03/heightofTree.java
@@ -1,24 +1,25 @@
 package com.company;
 
 public class heightofTree {
-    public int maxDepth(TreeNode root) {
+    public static int height(Node root) {
+        return getHeight(root)-1;
+
+
+    }
+    public static int getHeight(Node root){
         if(root==null){
             return 0;
         }
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
         return Math.max(left,right) + 1;
+
+
     }
 }
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
+class Node {
+    int data;
+    Node left;
+    Node right;
 }
+
