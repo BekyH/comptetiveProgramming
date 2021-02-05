4ca41a1e4773b35cb904d4198da8f77b9cc398ab
unknown <bereketheramo25@gmail.com>
2021-02-05 04:31:22 +0300

    counting negatives

---
+++ week04/countNegatives.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class countNegatives {
+    public int countNegatives(int[][] grid) {
+        int counter = 0;
+        for(int i = 0;i<grid.length;i++){
+            for(int j = 0;j<grid[i].length;j++){
+                if(grid[i][j]<0){
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+
+    }
+
+
