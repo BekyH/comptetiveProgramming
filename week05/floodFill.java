31c7c31e1579a2b1ffc7ed66163a2b4188b2e6bf
unknown <bereketheramo25@gmail.com>
2021-02-23 05:33:30 +0300

    floodfill

--- week05/floodFill.java
+++ week05/floodFill.java
@@ -7,37 +7,24 @@ public class floodFill {
         if(orginalcolor==newColor){
             return image;
         }
-        List<List<Integer>> positions = new ArrayList<>();
-        List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
-        List<Integer> list3 = new ArrayList<>();
-        List<Integer> list4 = new ArrayList<>();
-        list1.add(1);
-        list1.add(0);
-        list2.add(0);
-        list2.add(1);
-        list3.add(-1);
-        list3.add(0);
-        list4.add(0);
-        list4.add(-1);
-        positions.add(list1);
-        positions.add(list2);
-        positions.add(list3);
-        positions.add(list4);
+        int [] dx = {-1,0,1,0};
+        int [] dy = {0,1,0,-1};
         int numOfRow = image.length;
         int numOfCol = image[0].length;
-        image =floodFillHelper(image,orginalcolor,newColor,numOfRow,numOfCol,positions,sr,sc);
+        image =floodFillHelper(image,orginalcolor,newColor,numOfRow,numOfCol,dx,dy,sr,sc);
         return image;
     }
-    public int[][] floodFillHelper(int[][] image,int orginalcolor,int newColor,int num_row,int num_col,List<List<Integer>> positions,int sr,int sc){
+
+    public int[][] floodFillHelper(int[][] image,int orginalcolor,int newColor,int num_row,int num_col,int[]dx,int[]dy, int sr,int sc){
         image[sr][sc] = newColor;
 
-        for(List<Integer> position:positions){
-            int newrow = sr + position.get(0);
-            int newcol = sc + position.get(1);
+        for(int i = 0;i<4;i++){
+            int newrow = sr + dx[i];
+            int newcol = sc + dy[i];
+
             if((0<=newrow && newrow<num_row) && (0<=newcol && newcol<num_col)){
                 if(image[newrow][newcol]==orginalcolor){
-                    floodFillHelper(image,orginalcolor,newColor,num_row,num_col,positions,newrow,newcol);
+                    floodFillHelper(image,orginalcolor,newColor,num_row,num_col,dx,dy,newrow,newcol);
                 }
 
             }
