8c0938fdc64d23fdfbdcd3071de8a7795f4c0790
unknown <bereketheramo25@gmail.com>
2021-02-25 04:56:43 +0300

    small modifications

--- week05/floodFill.java
+++ week05/floodFill.java
@@ -7,24 +7,23 @@ public class floodFill {
         if(orginalcolor==newColor){
             return image;
         }
-        int [] dx = {-1,0,1,0};
-        int [] dy = {0,1,0,-1};
+        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
         int numOfRow = image.length;
         int numOfCol = image[0].length;
-        image =floodFillHelper(image,orginalcolor,newColor,numOfRow,numOfCol,dx,dy,sr,sc);
+        image =floodFillHelper(image,orginalcolor,newColor,numOfRow,numOfCol,directions,sr,sc);
         return image;
     }
 
-    public int[][] floodFillHelper(int[][] image,int orginalcolor,int newColor,int num_row,int num_col,int[]dx,int[]dy, int sr,int sc){
+    public int[][] floodFillHelper(int[][] image,int orginalcolor,int newColor,int num_row,int num_col,int[][]directions, int sr,int sc){
         image[sr][sc] = newColor;
 
-        for(int i = 0;i<4;i++){
-            int newrow = sr + dx[i];
-            int newcol = sc + dy[i];
+        for(int[] direction:directions){
+            int newrow = sr + direction[0];
+            int newcol = sc + direction[1];
 
             if((0<=newrow && newrow<num_row) && (0<=newcol && newcol<num_col)){
                 if(image[newrow][newcol]==orginalcolor){
-                    floodFillHelper(image,orginalcolor,newColor,num_row,num_col,dx,dy,newrow,newcol);
+                    floodFillHelper(image,orginalcolor,newColor,num_row,num_col,directions,newrow,newcol);
                 }
 
             }
@@ -35,3 +34,5 @@ public class floodFill {
     }
 
 }
+
+
--- week06/maxAreaofIsland.java
+++ week06/maxAreaofIsland.java
@@ -1,26 +1,34 @@
 public class maxAreaofIsland {
 
-    public int maxAreaIsland(int[][] grid) {
+    public int maxAreaOfIsland(int[][] grid) {
         int area = 0;
         int maxarea = 0;
-        for(int i = 0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
+        int[][] directions = {{1,0},{0,-1},{-1,0},{0,1}};
+        int m = grid.length;
+        int n = grid[0].length;
+        for(int i = 0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
-                    maxarea=dfs(grid,i,j);
-                    area = Math.max(area,maxarea);
+                    area=dfs(grid,i,j,directions,m,n);
+                    maxarea = Math.max(area,maxarea);
                 }
             }
         }
-        return area;
+        return maxarea;
     }
-    public int dfs(int[][] grid,int row,int col){
-        if(row<0 || row>=grid.length || col<0 || col>=grid[0].length || grid[row][col]==0){
-            return 0;
-        }
+    public int dfs(int[][] grid,int row,int col,int[][]directions,int m,int n){
+        int counter = 1;
         grid[row][col]=0;
-
-        return 1 +dfs(grid,row+1,col) +  dfs(grid,row-1,col) +  dfs(grid,row,col+1) + dfs(grid,row,col-1);
+        for(int[] direction:directions){
+            int newrow = row + direction[0];
+            int newcol = col + direction[1];
+            if(newrow>=0 && newrow<m && newcol>=0 && newcol<n && grid[newrow][newcol]==1){
+                counter+=dfs(grid,newrow,newcol,directions,m,n);
+            }
+        }
+        return counter;
     }
-
 }
+
+
 
