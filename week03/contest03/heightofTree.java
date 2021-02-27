95d6ca9f8d77b34ff81333ffff9f0ba764a867cb
unknown <bereketheramo25@gmail.com>
2021-02-27 03:31:50 +0300

    changes

--- week03/contest03/heightofTree.java
+++ week03/contest03/heightofTree.java
@@ -1,21 +1,44 @@
 package com.company;
 
 public class heightofTree {
-    public static int height(Node root) {
-        return getHeight(root)-1;
+//    public static int height(Node root) {
+//        return getHeight(root)-1;
+//
+//
+//    }
+//    public static int getHeight(Node root){
+//        if(root==null){
+//            return 0;
+//        }
+//        int left = getHeight(root.left);
+//        int right = getHeight(root.right);
+//        return Math.max(left,right) + 1;
+//
+//
+//    }
+public static int height(Node root) {
+    int h = getHeight(root);
+    return h;
 
 
-    }
-    public static int getHeight(Node root){
-        if(root==null){
-            return 0;
+}
+    public static int getHeight(Node node){
+
+        int leftHeight = 0;
+        int rightHeight = 0;
+        if(isLeafNode(node)){
+            return Math.max(leftHeight,rightHeight);
         }
-        int left = getHeight(root.left);
-        int right = getHeight(root.right);
-        return Math.max(left,right) + 1;
+        if(node.left!=null){
+            leftHeight = 1 + getHeight(node.left);
 
-
+        }
+        if(node.right!=null){
+            rightHeight = 1 + getHeight(node.right);
+        }
+        return Math.max(leftHeight,rightHeight);
     }
+
 }
 class Node {
     int data;
