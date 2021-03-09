f6d8f02be7283b289f9530ab6b86799624cd6a4b
unknown <bereketheramo25@gmail.com>
2021-03-09 15:28:16 +0300

    first

---
+++ bootcamp/week01/uniqueNumberOfOccurences.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class uniqueNumberOfOccurences {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer,Integer> frequencies = new HashMap<>();
+        HashSet<Integer> hashset = new HashSet<>();
+        for(int value:arr){
+            if(frequencies.containsKey(value)){
+                frequencies.put(value,frequencies.get(value) +1);
+            }
+            else{
+                frequencies.put(value,1);
+            }
+        }
+        for(Map.Entry<Integer,Integer> freq:frequencies.entrySet()){
+            hashset.add(freq.getValue());
+        }
+        if(hashset.size()==frequencies.size()){
+            return true;
+        }
+        return false;
+
+    }
+}
