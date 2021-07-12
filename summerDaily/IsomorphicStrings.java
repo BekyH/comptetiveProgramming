4f0f31e647aa7b719cd8197ff23522314a6dc2ba
bereketheramo25@gmail.com <bereketheramo25@gmail.com>
2021-07-12 10:51:45 +0300

    isomorphic

---
+++ summerDaily/IsomorphicStrings.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        StringBuilder sb = new StringBuilder();
+        HashSet<Character> mapped = new HashSet<>();
+        HashMap<Character,Character> hashmap = new HashMap<>();
+        for(int i=0;i<t.length();i++){
+            if(hashmap.containsKey(s.charAt(i))){
+                sb.append(hashmap.get(s.charAt(i)));
+            }
+            else{
+                if(mapped.contains(t.charAt(i))){
+                    break;
+                }
+                sb.append(t.charAt(i));
+                hashmap.put(s.charAt(i),t.charAt(i));
+                mapped.add(t.charAt(i));
+            }
+        }
+        String str = sb.toString();
+        if(t.equals(str)){
+            return true;
+        }
+        return false;
+        
+    }
+}
+}
