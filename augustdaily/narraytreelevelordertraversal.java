9e582f0e8bae71e98cb6a578e4e093214d8ae17e
BekyH <bereketheramo25@gmail.com>
2021-08-06 14:44:41 +0300

    narray tree level order traversal

---
+++ augustdaily/narraytreelevelordertraversal.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class narraytreelevelordertraversal{
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> res = new ArrayList<>();
+        Deque<Node> deque = new ArrayDeque<>();
+        List<Integer> list = new ArrayList<>();
+           if(root==null){
+               return res;
+           }
+           list.add(root.val);
+           res.add(list);
+           deque.add(root);
+           while(!deque.isEmpty()){
+               int size = deque.size();
+               List<Integer> lst = new ArrayList<>();
+               for(int i=0;i<size;i++){
+                Node n = deque.poll();
+                List<Node> ls = n.children;
+                 for(Node child:ls){
+                     lst.add(child.val);
+                     deque.add(child);
+                 }
+               }
+               if(lst.size()>0){
+                    res.add(lst);
+               }
+              
+           }
+           return res;
+       }
+   }
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
