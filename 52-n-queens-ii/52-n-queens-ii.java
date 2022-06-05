a26f1f35d1ec8ebc505ab86be8cd60fc4c5922cd
Bereket Heramo <32976671+BekyH@users.noreply.github.com>
2022-06-05 10:16:46 +0300

    Time: 6 ms (40.96%), Space: 41.3 MB (39.46%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -0,0 +1,58 @@
+class Solution {
+    public int totalNQueens(int n) {
+     Set<Integer> cols = new HashSet<>();
+     Set<Integer> pd = new HashSet<>();
+     Set<Integer> nd = new HashSet<>();
+     List<List<String>>result = new ArrayList<>();
+     String [][] board = new String[n][n];
+     for(String[] row:board){
+         Arrays.fill(row,".");
+         }
+        backtrack(board,n,0,result,pd,nd,cols);
+                return result.size();
+        
+    }
+    
+    public void backtrack(String[][] board,int n,int row,List<List<String>> result,Set<Integer> pd,Set<Integer> nd,Set<Integer> cols){
+        //System.out.println(row);
+
+        if(row==n){
+            addPath(board,n,result);
+            return;
+        }
+        
+        for(int col=0;col<n;col++){
+            int x = row+col;
+            int y = row-col;
+            if(cols.contains(col) || pd.contains(x) || nd.contains(y)){
+                continue;
+            }
+            
+            
+            cols.add(col);
+            pd.add(row+col);
+            nd.add(row-col);
+            board[row][col]="Q";
+            backtrack(board,n,row+1,result,pd,nd,cols);
+            cols.remove(col);
+            pd.remove(row+col);
+            nd.remove(row-col);
+            board[row][col]=".";
+        }
+        
+    }
+    public void addPath(String[][] board,int n,List<List<String>> result){
+         List<String> lst = new ArrayList<>();
+
+        for(int i=0;i<n;i++){
+            StringBuilder sb = new StringBuilder();
+                       for(int j=0;j<n;j++){
+                   sb.append(board[i][j]);
+               }
+            
+                lst.add(sb.toString());
+            
+            }
+        result.add(lst);
+    }
+}
