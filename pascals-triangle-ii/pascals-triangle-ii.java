03c4fa9ad2cca35ca7964a3620a975581ed04961
Bereket Heramo <32976671+BekyH@users.noreply.github.com>
2022-06-06 06:31:49 +0300

    Time: 8 ms, Memory: 44.1 MB - LeetHub

---
+++ pascals-triangle-ii/pascals-triangle-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> result = new ArrayList<>();
+        HashMap<List<Integer>,Integer> memo = new HashMap<>();
+        for(int i=0;i<=rowIndex;i++){
+            int val = pascal(rowIndex,i,memo);
+            result.add(val);
+        }
+        
+        return result;
+        
+    }
+    
+    public int pascal(int row, int col,HashMap<List<Integer>,Integer> memo){
+        if(row==0 || col==0 || row==col){
+            return 1;
+        }
+        List<Integer> lst = Arrays.asList(row,col);
+        if(memo.containsKey(lst)){
+            return memo.get(lst);
+        }
+        memo.put(lst,pascal(row-1,col,memo) + pascal(row-1,col-1,memo));
+        
+        return memo.get(lst);
+    }
+}
