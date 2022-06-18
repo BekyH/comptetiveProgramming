b16c8c2d93f63950d57e3218c970919712f2e962
Bereket Heramo <32976671+BekyH@users.noreply.github.com>
2022-06-18 10:05:41 +0300

    Time: 96 ms (69.35%), Space: 108.1 MB (34.89%) - LeetHub

---
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.java
@@ -0,0 +1,47 @@
+class Solution {
+    public String largestWordCount(String[] messages, String[] senders) {
+        HashMap<String,Integer> hashmap = new HashMap<>();
+        PriorityQueue<Node> heap = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<messages.length;i++){
+            String msg = messages[i];
+            String sender = senders[i];
+            int len = msg.split(" ").length;
+            if(hashmap.containsKey(sender)){
+                int val = hashmap.get(sender);
+                val = val + len;
+                hashmap.put(sender,val);
+            }
+            else{
+                hashmap.put(sender,len);
+            }
+        }
+        for(Map.Entry<String,Integer> e: hashmap.entrySet()){
+            String str = e.getKey();
+            int freq = e.getValue();
+            Node node = new Node(freq, str);
+            heap.add(node);
+        }
+        
+        return heap.peek().name;
+    }
+}
+
+class Node implements Comparable<Node>{
+    int count;
+    String name;
+    
+    Node(int count,String name){
+        this.count = count;
+        this.name = name;
+        
+    }
+    @Override
+    public int compareTo(Node node){
+        if(count==node.count){
+            return name.compareTo(node.name);
+        }
+        
+        return count-node.count;
+        
+    }
+}
