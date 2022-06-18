06ccb7eeb2cfd99a85fe9e80f87c6fce31bea1b0
Bereket Heramo <32976671+BekyH@users.noreply.github.com>
2022-06-18 09:34:35 +0300

    Time: 17 ms (56.53%), Space: 51.8 MB (8.46%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> hashmap = new HashMap<>();
+        PriorityQueue<Node> max_heap = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<nums.length;i++){
+            if(hashmap.containsKey(nums[i])){
+                int val = hashmap.get(nums[i]);
+                val = val +1;
+                hashmap.put(nums[i],val);
+            }
+            else{
+                hashmap.put(nums[i],1);
+            }
+        }
+        for(Map.Entry<Integer,Integer> e:hashmap.entrySet()){
+            Node node = new Node(e.getKey(), e.getValue());
+            max_heap.add(node);
+        }
+        int [] res = new int[k];
+        int index=0;
+        while(index<k){
+            res[index++] = max_heap.poll().val;
+        }
+        return res;
+    }
+}
+
+class Node implements Comparable<Node>{
+    int val;
+    int freq;
+    Node(int val,int freq){
+        this.val = val;
+        this.freq = freq;
+    }
+    @Override
+    public int compareTo(Node node){
+        return freq-node.freq;
+    }
+    
+}
