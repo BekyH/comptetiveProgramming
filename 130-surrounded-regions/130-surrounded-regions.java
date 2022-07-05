9962b2c10e82c4e1b0b5bf8d37717be13a38f3c6
Bereket Heramo <32976671+BekyH@users.noreply.github.com>
2022-07-05 22:10:42 +0300

    Time: 5 ms (26.05%), Space: 51.4 MB (68.77%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.java
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -2,45 +2,41 @@ class Solution {
     public void solve(char[][] board) {
         int m = board.length;
         int n = board[0].length;
-        boolean [][] invalid = new boolean[m][n];
-        int [][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+        int [][] directions = {{0,1},{0,-1},{1,0},{-1,0}};
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(i==0 || j==0 || i==m-1 || j==n-1){
                     if(board[i][j]=='O'){
-                        invalid[i][j] = true;
-                        dfs(board,i,j,directions,invalid);
+                        dfs(board,directions,i,j);
                     }
-                    
-                    
                 }
             }
         }
-       for(int i=0;i<m;i++){
-           for(int j=0;j<n;j++){
-               if(!invalid[i][j]){
-                   board[i][j] = 'X';
-               }
-              // System.out.print(invalid[i][j] + " ");
-           }
-           //System.out.println(" ");
-       }
-        
-        
-        
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j]=='O'){
+                    board[i][j] = 'X';
+                }
+            }
+        }
+         for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j]=='A'){
+                    board[i][j] = 'O';
+                }
+            }
+        }
         
     }
-    public void dfs(char[][] board,int row,int col,int [][] directions, boolean[][] invalid)
-    {
-         for(int [] direction: directions){
-             
-             int new_row = row+direction[0];
-             int new_col = col + direction[1];
-             
-             if(new_row>=0 && new_row<board.length && new_col>=0 && new_col<board[0].length && board[new_row][new_col]=='O' && !invalid[new_row][new_col]){
-                 invalid[new_row][new_col] = true;
-                 dfs(board,new_row,new_col,directions,invalid);
-             }
-         }
+    
+    public void dfs(char[][] board, int[][] directions,int i,int j){
+        board[i][j] = 'A';
+        for(int [] direction:directions){
+            int new_row = i + direction[0];
+            int new_col = j + direction[1];
+            if(new_row>=0 && new_col>=0 && new_row<board.length && new_col<board[0].length && board[new_row][new_col]=='O'){
+               dfs(board,directions,new_row,new_col); 
+            }
+        }
     }
 }
