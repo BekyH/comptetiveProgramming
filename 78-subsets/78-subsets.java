aa6dfac84a250aaba29ad9193acd7a8f4da30b55
Bereket Heramo <32976671+BekyH@users.noreply.github.com>
2022-07-11 16:30:01 +0300

    Time: 2 ms (38.73%), Space: 43.3 MB (59.50%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,20 +1,22 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-              List<List<Integer>> subsets = new ArrayList<>();
-               subset_generator(subsets, new ArrayList<>(),0,nums); 
-                return subsets;
+        List<List<Integer>> result = new ArrayList<>();
+        
+        helper(new ArrayList<>(),nums,0,result);
+        return result;
+        
     }
-    public void subset_generator(List<List<Integer>> list,List<Integer> result,int i,int[] nums){
-        
-            list.add(new ArrayList<>(result));
-        
-         
-        for(int j=i;j<nums.length;j++){
-            result.add(nums[j]);
-            subset_generator(list,result,j+1,nums);
-            result.remove(result.size()-1);
+    
+    public void helper(List<Integer> path, int[] nums,int index,List<List<Integer>> result){
+     
+        result.add(new ArrayList<>(path));
+        for(int i=index;i<nums.length;i++){
+            
+            path.add(nums[i]);
+            helper(path,nums,i+1,result);
+            
+            path.remove(path.size()-1);
             
         }
     }
-
 }
