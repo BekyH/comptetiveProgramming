01555c341e3668f227d688bf2025075dff4efc8a
Bereket Heramo <32976671+BekyH@users.noreply.github.com>
2022-07-13 09:23:41 +0300

    Time: 2 ms (96.16%), Space: 42.6 MB (73.67%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -1,41 +1,37 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int totalmin =0;
+        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+        int freshcount = 0;
+        int min = 0;
         Deque<int[]> deque = new ArrayDeque<>();
-        int [][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]==2){
-                    deque.add(new int[]{i,j,0});
+                     deque.add(new int[]{i,j,0});
+                }
+                else if(grid[i][j]==1){
+                      freshcount++;
                 }
             }
         }
         
         while(!deque.isEmpty()){
-            int[] cell = deque.poll();
-            int row =cell[0];
-            int col = cell[1];
-            int min = cell[2];
-            totalmin = min;
-            for(int[] direction:directions){
-                int new_row = row + direction[0];
-                int new_col = col + direction[1];
-                if(new_row>=0 && new_row<m && new_col>=0 && new_col<n && grid[new_row][new_col]==1){
-                    grid[new_row][new_col] = 2;
-                    deque.add(new int[]{new_row,new_col,min+1});
-                }
-            }
-        }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1){
-                  return -1; 
+            int [] curr = deque.poll();
+            int curr_row = curr[0];
+            int curr_col = curr[1];
+            int curr_min = curr[2];
+            min = curr_min;
+            for(int [] direction:directions){
+                 int row = curr_row + direction[0];
+                 int col = curr_col + direction[1];
+                if(row>=0 && row<grid.length && col>=0 && col<grid[0].length && grid[row][col]==1){
+                    grid[row][col] = 2;
+                    freshcount--;
+                    deque.add(new int[]{row,col,curr_min+1});
                 }
             }
         }
         
-        return totalmin;
+        return freshcount==0? min:-1;
     }
 }
