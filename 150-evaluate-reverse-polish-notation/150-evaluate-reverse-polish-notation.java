db7860967c356fcefefd9b21c00821a162d2c854
Bereket Heramo <32976671+BekyH@users.noreply.github.com>
2022-07-21 06:50:10 +0300

    Time: 7 ms (80.40%), Space: 44 MB (57.35%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -1,47 +1,38 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> stack = new Stack<>();
-        
-        for(int i=0;i<tokens.length;i++){
-            String str = tokens[i];
-            int z = 0;
-            
-            if(str.equals("+")){
-                 int y = stack.pop();
-                 int x = stack.pop();
-                    z = x + y;
-                    stack.push(z);
-                }
-                else if(str.equals("-")){
-                     int y = stack.pop();
-                     int x = stack.pop();
-                    z = x-y;
-                    stack.push(z);
-                    
-                }
-                else if(str.equals("*")){
-                     int y = stack.pop();
-                      int x = stack.pop();
-                    z = x * y;
-                    stack.push(z);
-                    
-                }
-                else if(str.equals("/")){
-                     int y = stack.pop();
-                      int x = stack.pop();
-                   // System.out.println(stack);
-                    z = x/y;
-                    stack.push(z);
-                    
-                }
-                else{
-                    stack.push(Integer.parseInt(str));
-                }
-                
+        Stack<Integer> st = new Stack<>();
+        for(String str:tokens){
+            switch(str){
+                case "+":
+                    int x = st.pop();
+                    int y = st.pop();
+                    int z= x+y;
+                    st.push(z);
+                    break;
+                case "-":
+                     int x1 = st.pop();
+                    int y1 = st.pop();
+                    int z1= y1-x1;
+                    st.push(z1);
+                    break;
+                case "*":
+                     int x2 = st.pop();
+                    int y2 = st.pop();
+                    int z2= x2*y2;
+                    st.push(z2);
+                    break;
+                case "/":
+                     int x3 = st.pop();
+                    int y3 = st.pop();
+                    int z3= y3/x3;
+                    st.push(z3);
+                    break;
+                default:
+                    int num = Integer.parseInt(str);
+                    st.push(num);
+                    break;
             }
-                
-        
-        
-        return stack.peek();
+        }
+        return st.peek();
     }
 }
