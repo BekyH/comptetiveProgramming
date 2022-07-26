ec181bac548e288664756a076a8fb71fab4cb110
Bereket Heramo <32976671+BekyH@users.noreply.github.com>
2022-07-26 10:50:57 +0300

    Time: 38 ms (62.10%), Space: 54.9 MB (39.12%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,44 +1,55 @@
 class Solution {
     public int shortestPath(int[][] grid, int k) {
-        int [][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
-        Deque<int[]> deque = new ArrayDeque<>();
         int m = grid.length;
         int n = grid[0].length;
-        boolean[][][]visited = new boolean[m][n][k+1];
-        int [] start = {0,0,k,0};
-        
+         boolean [][][] visited = new boolean[m][n][k+1];
+        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+        Deque<Node> deque = new ArrayDeque<>();
+        Node start = new Node(0,0,k,0);
         deque.add(start);
-        visited[0][0][k] = true;
         while(!deque.isEmpty()){
-            int [] res = deque.poll();
-            int r = res[0];
-            int c = res[1];
-            int k_amount = res[2];
-            int step = res[3];
-            if(r==m-1 && c==n-1){
+            Node node = deque.poll();
+            int row = node.row;
+            int col = node.col;
+            int k_ = node.k;
+            int step = node.step;
+            
+            if(row==m-1 && col==n-1 ){
                 return step;
             }
-            for(int [] direction:directions){
-                int new_row = r + direction[0];
-                int new_col = c + direction[1];
+            for(int [] dir:directions){
+                int new_row = row + dir[0];
+                int new_col = col + dir[1];
                 if(new_row>=0 && new_row<m && new_col>=0 && new_col<n){
-                    
-                    if(!visited[new_row][new_col][k_amount] && grid[new_row][new_col]==0){
-                        int [] curr = {new_row,new_col,k_amount,step+1};
-                        deque.add(curr);
-                        visited[new_row][new_col][k_amount] = true;
-                        
-                    }
-                    else if(grid[new_row][new_col]==1 && k_amount>0 && !visited[new_row][new_col][k_amount-1]){
-                         int [] curr = {new_row,new_col,k_amount-1,step+1};
-                          deque.add(curr);
-                          visited[new_row][new_col][k_amount-1] = true;
-                        
+                     if(grid[new_row][new_col]==0 && !visited[new_row][new_col][k_]){
+                         Node child = new Node(new_row,new_col,k_,step+1);
+                         deque.add(child);
+                         visited[new_row][new_col][k_] = true;
+                     }
+                    else if(grid[new_row][new_col]==1 && k_-1>=0 && !visited[new_row][new_col][k_-1]){
+                         Node newchild = new Node(new_row,new_col,k_-1,step+1);
+                         deque.add(newchild);
+                         visited[new_row][new_col][k_-1] = true;
+                         
                     }
                 }
             }
+            
         }
         
         return -1;
+    }
+}
+
+class Node{
+    int row;
+    int col;
+    int k;
+    int step;
+    Node(int row,int col,int k,int step){
+        this.row = row;
+        this.col = col;
+        this.k = k;
+        this.step = step;
     }
 }
