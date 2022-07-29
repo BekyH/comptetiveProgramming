6af2ae309c1e213a5e66cc6c3e64c4325f4989ee
Bereket Heramo <32976671+BekyH@users.noreply.github.com>
2022-07-29 09:20:17 +0300

    Time: 0 ms (100.00%), Space: 41.5 MB (54.46%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -4,22 +4,19 @@ class Solution {
         if(n==1){
             return nums[0];
         }
-        
+        if(n==2){
+            return Math.max(nums[0],nums[1]);
+        }
         for(int i=2;i<n;i++){
-              if(i-3<0){
-                  nums[i] = nums[i] + nums[i-2];
-              }
-             else{
-                 nums[i] = nums[i] + Math.max(nums[i-3],nums[i-2]);
-             }
-            
-            
+            if(i==2){
+                nums[i] = nums[i] + nums[i-2];
+            }
+            else{
+            nums[i] = nums[i] + Math.max(nums[i-3],nums[i-2]);
+                
+            }
         }
         
         return Math.max(nums[n-1],nums[n-2]);
-        
-        
-        
-     
     }
 }
