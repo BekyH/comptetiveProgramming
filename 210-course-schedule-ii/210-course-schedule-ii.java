2b42a2d78caefbd5cb22d11aebfb520b74a0a575
Bereket Heramo <32976671+BekyH@users.noreply.github.com>
2022-07-31 16:12:13 +0300

    Time: 10 ms (54.32%), Space: 49.7 MB (41.60%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,40 +1,48 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-         HashMap<Integer,List<Integer>> map = new HashMap<>();
-        int [] indegree = new int[numCourses];
-        int [] result =new int[numCourses];
-        for(int i=0;i<numCourses;i++){
-            map.put(i,new ArrayList<>());
-        
-        }
-        for(int [] edge:prerequisites){
-            map.get(edge[1]).add(edge[0]);
-            indegree[edge[0]]++;
-        }
-        
-       
+        HashMap<Integer,List<Integer>> graph = new HashMap<>();
         Deque<Integer> deque = new ArrayDeque<>();
+        int n = numCourses;
+        int [] indegree = new int[n];
+        int [] result = new int[n];
         int index = 0;
-        for(int i=0;i<numCourses;i++){
+        for(int i=0;i<n;i++){
+            graph.put(i,new ArrayList<>());
             
+        }
+        for(int [] prerequisite:prerequisites){
+            graph.get(prerequisite[1]).add(prerequisite[0]);
+            
+            indegree[prerequisite[0]]++;
+        }
+        
+        for(int i=0;i<n;i++){
             if(indegree[i]==0){
-                //System.out.println(i + ":" + indegree[i]);
+               // System.out.println(i);
                 deque.add(i);
             }
         }
+                     
         while(!deque.isEmpty()){
-            int node = deque.poll();
-          //  System.out.println(node);
-            result[index++] = node;
-            //num++;
-            List<Integer> children = map.get(node);
+          //  System.out.println("e");
+            int current = deque.poll();
+            result[index++] = current;
+            List<Integer> children = graph.get(current);
             for(int child:children){
-                indegree[child]--;
+              indegree[child]--;
+               // System.out.println(indegree[child]);
                 if(indegree[child]==0){
                     deque.add(child);
                 }
             }
         }
-        return index==numCourses? result: new int[0];
+                     
+        for(int i:indegree){
+            if(i>0){
+                return new int[0];
+            }
+        }
+                     
+        return result;
     }
 }
