a0bd0835f31108e244efdc6ca604a4751ed95a17
Bereket Heramo <32976671+BekyH@users.noreply.github.com>
2022-07-31 10:14:18 +0300

    Time: 5 ms (14.59%), Space: 48.3 MB (12.25%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -1,49 +1,32 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> list = new ArrayList<>();
-        for(int [] arr:intervals){
-            list.add(arr);
-        }
-        list.add(newInterval);
-        int[][] updated = list.toArray(new int[list.size()][2]);
-        
-        return mergeInterval(updated);
-        
-    }
-    public int[][] mergeInterval(int[][] updated){
-        Arrays.sort(updated,(a,b)->a[0]-b[0]);
-         List<int[]> result = new ArrayList<>();
-        
-         int prev_start = updated[0][0];
-         int prev_end = updated[0][1];
-        
-        
-         for(int i=1;i<updated.length;i++){
-             int [] curr = updated[i];
-              
-              if(curr[0]>prev_end){
-                 
-                  result.add(new int[]{prev_start,prev_end});
-                  prev_start = curr[0];
-                  prev_end = curr[1];
-              }
-            else if(curr[0]>=prev_start && curr[0]<=prev_end){
-                
-                 int [] merged = {prev_start,Math.max(prev_end,curr[1])};
-               
-                  prev_start = merged[0];
-                  prev_end = merged[1];
-                 
-             }
-
+        List<int[]> newIntervals = new ArrayList<>();
+        List<int[]> result = new ArrayList<>();
+        for(int[] interval:intervals){
+        newIntervals.add(interval);
             
-         }
-        result.add(new int[]{prev_start,prev_end});
-        int [][] res = new int[result.size()][2];
-        for(int i=0;i<result.size();i++){
-            res[i] = result.get(i);
         }
+        newIntervals.add(newInterval);
         
-        return res;
+        int[][] newInter = newIntervals.toArray(new int[newIntervals.size()][2]);
+        Arrays.sort(newInter,(a,b)->a[0]-b[0]);
+        int prevStart = newInter[0][0];
+        int prevEnd = newInter[0][1];
+        
+        for(int i=1;i<newInter.length;i++){
+            int [] current = newInter[i];
+            if(prevEnd<current[0]){
+                result.add(new int[]{prevStart,prevEnd});
+                prevStart= current[0];
+                prevEnd = current[1];
+            }
+            else{
+                prevStart = Math.min(prevStart,current[0]);
+                prevEnd = Math.max(prevEnd,current[1]);
+            }
+        }
+                result.add(new int[]{prevStart,prevEnd});
+        
+        return result.toArray(new int[result.size()][2]);
     }
 }
