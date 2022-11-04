77b7ba80e6bb501c070a00acbac5d54187c7afa8
Bereket Heramo <32976671+BekyH@users.noreply.github.com>
2022-11-04 08:49:38 +0300

    Time: 8 ms (5.23%), Space: 44.6 MB (83.85%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -0,0 +1,81 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+       
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+    public int maxPathSum(TreeNode root) {
+        List<Integer> pair = helper(root);
+        return pair.get(0);
+    }
+    
+    public List<Integer> helper(TreeNode root){
+        List<Integer> leftpair;
+        int path=0;
+        List<Integer> rightpair;
+        List<Integer> pair;
+        int max = Integer.MIN_VALUE;
+        if(isLeaf(root)){
+             pair = new ArrayList<>(Arrays.asList(root.val,root.val));
+            return pair;
+        }
+        else if(root.left!=null && root.right!=null){
+            
+            leftpair =helper(root.left);
+            //rightpair = new Pair<>();
+            rightpair=helper(root.right);
+        max = Collections.max(Arrays.asList(leftpair.get(0),max,root.val + leftpair.get(1),root.val,rightpair.get(0),root.val + rightpair.get(1),leftpair.get(1) + rightpair.get(1) + root.val));
+        
+         path = Collections.max(Arrays.asList(root.val,root.val + leftpair.get(1),root.val + rightpair.get(1)));
+        }
+        else if(root.right!=null){
+            //rightpair = new Pair<>();
+            rightpair = helper(root.right);
+             max = Collections.max(Arrays.asList(root.val,rightpair.get(0),root.val + rightpair.get(1)));
+           path = Math.max(root.val,rightpair.get(1) + root.val);                        
+        }
+        else{
+            //leftpair = new Pair<>();
+            leftpair = helper(root.left);
+           max = Collections.max(Arrays.asList(root.val,leftpair.get(0),root.val + leftpair.get(1)));
+           path = Math.max(root.val,leftpair.get(1) + root.val);                         
+        }                           
+        pair = Arrays.asList(max,path);
+            return pair;
+
+    }
+    public boolean isLeaf(TreeNode node){
+        if(node.left==null && node.right==null){
+            return true;
+        }
+        return false;
+    }
+}
+
