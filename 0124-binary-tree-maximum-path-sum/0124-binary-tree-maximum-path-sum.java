7f0f2acce0d530de9ffb032489038acd4ebe484e
Bereket Heramo <32976671+BekyH@users.noreply.github.com>
2022-11-04 12:06:51 +0300

    Time: 15 ms (5.23%), Space: 55 MB (5.29%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -14,68 +14,26 @@
  * }
  */
 class Solution {
-       
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-
     public int maxPathSum(TreeNode root) {
-        List<Integer> pair = helper(root);
-        return pair.get(0);
-    }
-    
-    public List<Integer> helper(TreeNode root){
-        List<Integer> leftpair;
-        int path=0;
-        List<Integer> rightpair;
-        List<Integer> pair;
-        int max = Integer.MIN_VALUE;
-        if(isLeaf(root)){
-             pair = new ArrayList<>(Arrays.asList(root.val,root.val));
-            return pair;
-        }
-        else if(root.left!=null && root.right!=null){
-            
-            leftpair =helper(root.left);
-            //rightpair = new Pair<>();
-            rightpair=helper(root.right);
-        max = Collections.max(Arrays.asList(leftpair.get(0),max,root.val + leftpair.get(1),root.val,rightpair.get(0),root.val + rightpair.get(1),leftpair.get(1) + rightpair.get(1) + root.val));
+        int [] g_max = new int[1];
+        g_max[0] = Integer.MIN_VALUE;
+        dfs(root,g_max);
+        return g_max[0];
         
-         path = Collections.max(Arrays.asList(root.val,root.val + leftpair.get(1),root.val + rightpair.get(1)));
-        }
-        else if(root.right!=null){
-            //rightpair = new Pair<>();
-            rightpair = helper(root.right);
-             max = Collections.max(Arrays.asList(root.val,rightpair.get(0),root.val + rightpair.get(1)));
-           path = Math.max(root.val,rightpair.get(1) + root.val);                        
-        }
-        else{
-            //leftpair = new Pair<>();
-            leftpair = helper(root.left);
-           max = Collections.max(Arrays.asList(root.val,leftpair.get(0),root.val + leftpair.get(1)));
-           path = Math.max(root.val,leftpair.get(1) + root.val);                         
-        }                           
-        pair = Arrays.asList(max,path);
-            return pair;
-
     }
-    public boolean isLeaf(TreeNode node){
-        if(node.left==null && node.right==null){
-            return true;
+    public int dfs(TreeNode node,int [] g_max){
+        if(node==null){
+            return 0;
         }
-        return false;
+        int left = 0;
+        int right = 0;
+        
+        left = dfs(node.left,g_max);
+        right =dfs(node.right,g_max);
+        
+        g_max[0]= Collections.max(Arrays.asList(g_max[0],node.val,left+node.val,right+node.val,left+right+node.val));
+        return Collections.max(Arrays.asList(node.val,node.val+right,node.val+left));
+        
+        
     }
 }
-
