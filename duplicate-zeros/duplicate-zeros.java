93f5df2e7da21438acee286d7bab4eb1d07df777
Bereket Heramo <32976671+BekyH@users.noreply.github.com>
2022-12-02 17:06:58 +0300

    Time: 6 ms, Memory: 45.4 MB - LeetHub

--- duplicate-zeros/duplicate-zeros.java
+++ duplicate-zeros/duplicate-zeros.java
@@ -1,18 +1,27 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==0){
-                helper(i,arr);
-                if(i+1<arr.length){
-                    arr[i+1] = 0;
-                    i++;
-                }
-            }
+        int n = arr.length;
+        int[] res = new int[n];
+        int index = 0;
+        for (int i=0;i<n;i++){
+              if(index>=n){
+                  break;
+              }
+              res[index] = arr[i];
+              
+              if(arr[i]==0){
+                  index++;
+              }
+              
+            index++;
+              
         }
-    }
-    public void helper(int i,int [] arr){
-        for(int j=arr.length-2;j>i;j--){
-            arr[j+1] = arr[j];
+        for(int i=0;i<n;i++){
+            arr[i] = res[i];
         }
+            
+        
+            
+        } 
+        
     }
-}
