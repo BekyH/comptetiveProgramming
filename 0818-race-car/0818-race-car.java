c5ec20f08076460d35799204716d4af139f530ae
Bereket Heramo <32976671+BekyH@users.noreply.github.com>
2023-02-06 19:58:00 +0300

    Time: 7 ms (86.71%), Space: 41.6 MB (76.21%) - LeetHub

--- 0818-race-car/0818-race-car.java
+++ 0818-race-car/0818-race-car.java
@@ -1,54 +1,35 @@
 class Solution {
     public int racecar(int target) {
-        Deque<car> deque =  new ArrayDeque<>();
-        Set<Pair<Integer,Integer>> visited = new HashSet<>();
-        car start = new car(0,1,0);
-        deque.add(start);
-        visited.add(new Pair(0,1));
+        Deque<int[]> deque = new ArrayDeque<>();
+        int [] inital  = {1,0,0};
+        deque.add(inital);
         while(!deque.isEmpty()){
-            car c = deque.poll();
-            int currpos = c.pos;
-            int currsp = c.speed;
-            int step = c.step;
-            if(currpos==target){
-                return step;
+             int [] curr = deque.poll();
+            int sp = curr[0];
+            int pos = curr[1];
+            int len = curr[2];
+            if(pos==target){
+                return len;
             }
             
-            int accpos = currpos + currsp;
-            int accsp = currsp * 2;
-            int revpos = currpos;
-            int revsp = 1;
-            if(currsp>0){
-                revsp = -1;
+            deque.add(new int[]{sp*2,pos+sp,len+1});
+            if(((pos+sp)>target && sp>0) || ((pos+sp)< target && sp<0)){
+                if(sp>0){
+                    sp = -1;
+                }
+                else{
+                    sp = 1;
+                }
+                deque.add(new int[]{sp,pos,len+1});
+             
             }
-            Pair<Integer,Integer> p = new Pair(accpos,accsp);
-            if(!visited.contains(p) && accpos<=2*target && accpos>=0){
-            car acccar = new car(accpos,accsp,step+1);
-            deque.add(acccar);
-             visited.add(p);
+            else{
+                continue;
             }
-            Pair<Integer,Integer> p2 = new Pair(revpos,revsp);
-           
-            if(!visited.contains(p2) && revpos<=2*target && revpos>=0){
-            car revcar = new car(revpos,revsp,step+1);
-            deque.add(revcar);
-                
-            visited.add(p2);
-            }
-      
+            
+            
         }
         
-        return 1;
-    }
-    
-}
-class car{
-    int pos;
-    int speed;
-    int step;
-    car(int pos,int speed,int step){
-        this.pos = pos;
-        this.speed = speed;
-        this.step = step;
+        return -1;
     }
 }
